014ec8a031a200f1f37ec2948b51b958a4642051
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-30 10:27:43 -0800

    Adding Seokhas unit tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -451,6 +451,30 @@ int main()
         cout<<"====================="<<endl;
         cout<<endl;
     
+	cout << "======================" << endl;
+        cout << "Create Playlist Test" << endl;
+        cout << "======================" << endl;
+        cout << endl;
+        User_Menu* menu = new User_Menu();
+        menu->public_add_playlist();
+        menu->public_access_playlist();
+        cout << endl;
+        cout << "======================" << endl;
+        cout << "Create Playlist Test Finished" << endl;
+        cout << "======================" << endl;
+        cout << endl;
+        cout << "======================" << endl;
+        cout << "Delete a Song in Playlist Test" << endl;
+        cout << "======================" << endl;
+        Public_Playlist* playlist = new Public_Playlist("New Playlist");
+        playlist->add_song("Happy birthday", "anonymouse", 2);
+        playlist->display_songs();
+        playlist->delete_song();
+        playlist->display_songs();
+        cout << "======================" << endl;
+        cout << "Delete a Song in Playlist Test Finished" << endl;
+        cout << "======================" << endl;
+	cout<<endl;
 
     cout<<endl;
     cout<<"====================="<<endl;
