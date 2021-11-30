6eba553a249fc24a04313e8273940b79ecf7b178
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-29 17:52:56 -0800

    Update User_Menu.hpp

--- header/User_Menu.hpp
+++ header/User_Menu.hpp
@@ -19,17 +19,17 @@ private:
 public:
 	~User_Menu();
 	void display_options(int);
-	//void public_add_playlist();
-	//Public_Playlist* public_create_new_playlist();
+	void public_add_playlist();
+	Public_Playlist* public_create_new_playlist();
 	void public_delete_playlist();
 	void public_access_playlist();
-	void public_create_new_playlist();
+// 	void public_create_new_playlist();
 		
-	//void private_add_playlist();
-	//Private_Playlist* private_create_new_playlist();
+	void private_add_playlist();
+	Private_Playlist* private_create_new_playlist();
 	void private_delete_playlist();
 	void private_access_playlist();
-	void private_create_new_playlist();
+// 	void private_create_new_playlist();
 	
 	int access_playlist(int,int); //testing purposes only
 	void add_playlist(); //testing purposes only
